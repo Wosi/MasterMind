8794e1427cd889b13e0f9d8fc894c4d136f13ede
Wosi <christopher.wos@gmail.com>
2017-11-12 14:17:11 +0100

    Refactored TMasterMindGuessEvaluator

--- MasterMind.Evaluator.pas
+++ MasterMind.Evaluator.pas
@@ -11,76 +11,115 @@ type
   TMasterMindGuessEvaluator = class(TInterfacedObject, IGuessEvaluator)
   private
     function ColorInCodeButWasNotAlreadyMarkedAsCorrectMatch(const Guess, CodeToBeGuessed: TMasterMindCode;
-      const CurrentEvaluationResult: TMasterMindGuessEvaluationResult; const ColorIndex: Integer): Boolean;
-      procedure SortResult(var EvaluationResult: TMasterMindGuessEvaluationResult);
-      function CountExactMatches(const EvaluationResult: TMasterMindGuessEvaluationResult): Integer;
-      function CountWrongPlaces(const EvaluationResult: TMasterMindGuessEvaluationResult): Integer;
-      function CountElements(const EvaluationResult: TMasterMindGuessEvaluationResult; const HintType: TMasterMindHint): Integer;
+      const GuessIndex: Integer): Boolean;
+    function GuessedColorIsAtCurrentPositionButIsNoCorrectMatch(const Guess, CodeToBeGuessed: TMasterMindCode;
+      const CurrentPosition, GuessPosition: Integer): Boolean;
+    function ColorCorrectAtPosition(const Guess, CodeToBeGuessed: TMasterMindCode; const Position: Integer): Boolean;
+    function ColorCorrectAtAnyPosition(const Guess, CodeToBeGuessed: TMasterMindCode; const Positions: array of Integer): Boolean;
+    procedure InitializeResultWithNoMatches(out EvaluationResult: TMasterMindGuessEvaluationResult);
+    procedure SetCorrectMatchesInResult(var EvaluationResult: TMasterMindGuessEvaluationResult; const CodeToBeGuessed, Guess: TMasterMindCode);
+    procedure SetWrongPlacesInResult(var EvaluationResult: TMasterMindGuessEvaluationResult; const CodeToBeGuessed, Guess: TMasterMindCode);
+    procedure SortResult(var EvaluationResult: TMasterMindGuessEvaluationResult);
+    function CountExactMatches(const EvaluationResult: TMasterMindGuessEvaluationResult): Integer;
+    function CountWrongPlaces(const EvaluationResult: TMasterMindGuessEvaluationResult): Integer;
+    function CountElements(const EvaluationResult: TMasterMindGuessEvaluationResult; const HintType: TMasterMindHint): Integer;
+    procedure SetMatchesAndWrongPlaces(var EvaluationResult: TMasterMindGuessEvaluationResult; const Matches, WrongPlaces: Integer);
   public
     function EvaluateGuess(const CodeToBeGuessed, Guess: TMasterMindCode): TMasterMindGuessEvaluationResult;
   end;
 
 implementation
 
-uses
-  EnumHelper;
-
-function CompareHint(const Left, Right: TMasterMindHint): Integer;
+function TMasterMindGuessEvaluator.EvaluateGuess(const CodeToBeGuessed, Guess: TMasterMindCode): TMasterMindGuessEvaluationResult;
 begin
-  if Left > Right then
-    Result := 1
-  else if Left < Right then
-    Result := -1
-  else
-    Result := 0;
-
-  WriteLn(TEnumHelper<TMasterMindHint>.EnumToStr(Left), ' <-> ', TEnumHelper<TMasterMindHint>.EnumToStr(Right), ' = ', Result);
+  InitializeResultWithNoMatches(Result);
+  SetCorrectMatchesInResult(Result, CodeToBeGuessed, Guess);
+  SetWrongPlacesInResult(Result, CodeToBeGuessed, Guess);
+  SortResult(Result);
 end;
 
-function TMasterMindGuessEvaluator.EvaluateGuess(const CodeToBeGuessed, Guess: TMasterMindCode): TMasterMindGuessEvaluationResult;
+procedure TMasterMindGuessEvaluator.InitializeResultWithNoMatches(out EvaluationResult: TMasterMindGuessEvaluationResult);
 var
   I: Integer;
 begin
   for I := Low(TMasterMindCode) to High(TMasterMindCode) do
-    Result[I] := mmhNoMatch;
-
-  for I := Low(TMasterMindCode) to High(TMasterMindCode) do
-    if CodeToBeGuessed[I] = Guess[I] then
-      Result[I] := mmhCorrect;
-
-  for I := Low(TMasterMindCode) to High(TMasterMindCode) do
-    if ColorInCodeButWasNotAlreadyMarkedAsCorrectMatch(Guess, CodeToBeGuessed, Result, I) then
-      Result[I] := mmhWrongPlace;
-
-  SortResult(Result);
+    EvaluationResult[I] := mmhNoMatch;
 end;
 
+procedure TMasterMindGuessEvaluator.SetCorrectMatchesInResult(var EvaluationResult: TMasterMindGuessEvaluationResult;
+  const CodeToBeGuessed, Guess: TMasterMindCode);
+var
+  I: Integer;
+begin
+  for I := Low(TMasterMindCode) to High(TMasterMindCode) do
+    if CodeToBeGuessed[I] = Guess[I] then
+      EvaluationResult[I] := mmhCorrect;
+end;
+
+procedure TMasterMindGuessEvaluator.SetWrongPlacesInResult(var EvaluationResult: TMasterMindGuessEvaluationResult;
+  const CodeToBeGuessed, Guess: TMasterMindCode);
+var
+  I: Integer;
+begin
+  for I := Low(TMasterMindCode) to High(TMasterMindCode) do
+    if ColorInCodeButWasNotAlreadyMarkedAsCorrectMatch(Guess, CodeToBeGuessed, I) then
+      EvaluationResult[I] := mmhWrongPlace;
+end;
+
+
 function TMasterMindGuessEvaluator.ColorInCodeButWasNotAlreadyMarkedAsCorrectMatch(const Guess, CodeToBeGuessed: TMasterMindCode;
-  const CurrentEvaluationResult: TMasterMindGuessEvaluationResult; const ColorIndex: Integer): Boolean;
+  const GuessIndex: Integer): Boolean;
 var
   I: Integer;
 begin
   Result := False;
   for I := Low(TMasterMindCode) to High(TMasterMindCode) do
-    if (CodeToBeGuessed[I] = Guess[ColorIndex]) and (CodeToBeGuessed[I] <> Guess[I]) and (CodeToBeGuessed[ColorIndex] <> Guess[ColorIndex]) then
+    if GuessedColorIsAtCurrentPositionButIsNoCorrectMatch(Guess, CodeToBeGuessed, I, GuessIndex) then
       Exit(True);
+end;
+
+function TMasterMindGuessEvaluator.GuessedColorIsAtCurrentPositionButIsNoCorrectMatch(const Guess, CodeToBeGuessed: TMasterMindCode;
+  const CurrentPosition, GuessPosition: Integer): Boolean;
+begin
+  Result := (CodeToBeGuessed[CurrentPosition] = Guess[GuessPosition])
+    and (not ColorCorrectAtAnyPosition(CodeToBeGuessed, Guess, [CurrentPosition, GuessPosition]));
+end;
+
+function TMasterMindGuessEvaluator.ColorCorrectAtAnyPosition(const Guess, CodeToBeGuessed: TMasterMindCode;
+  const Positions: array of Integer): Boolean;
+var
+  Position: Integer;
+begin
+  Result := False;
+  for Position in Positions do
+    if ColorCorrectAtPosition(Guess, CodeToBeGuessed, Position) then
+      Exit(True);
+end;
+
+function TMasterMindGuessEvaluator.ColorCorrectAtPosition(const Guess, CodeToBeGuessed: TMasterMindCode; const Position: Integer): Boolean;
+begin
+  Result := CodeToBeGuessed[Position] = Guess[Position];
 end;
 
 procedure TMasterMindGuessEvaluator.SortResult(var EvaluationResult: TMasterMindGuessEvaluationResult);
 var
-  Matches, WrongPlaces, I: Integer;
+  Matches, WrongPlaces: Integer;
 begin
   Matches := CountExactMatches(EvaluationResult);
   WrongPlaces := CountWrongPlaces(EvaluationResult);
+  InitializeResultWithNoMatches(EvaluationResult);
+  SetMatchesAndWrongPlaces(EvaluationResult, Matches, WrongPlaces);
+end;
 
+procedure TMasterMindGuessEvaluator.SetMatchesAndWrongPlaces(var EvaluationResult: TMasterMindGuessEvaluationResult; const Matches, WrongPlaces: Integer);
+var
+  I: Integer;
+begin
   for I := 0 to Matches - 1 do
     EvaluationResult[I] := mmhCorrect;
 
   for I := 0 to WrongPlaces - 1 do
     EvaluationResult[Matches + I] := mmhWrongPlace;
-
-  for I := Matches + WrongPlaces to High(EvaluationResult) do
-    EvaluationResult[I] := mmhNoMatch;
 end;
 
 function TMasterMindGuessEvaluator.CountExactMatches(const EvaluationResult: TMasterMindGuessEvaluationResult): Integer;
